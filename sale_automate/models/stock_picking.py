40aa960d7c57916b186e05f8233cf8085100271a
AntoineSadzot <Sadzot.Antoine@gmail.com>
2021-01-29 11:58:28 +0100

    [FIX][2418403] Fix bug backorder generation

--- sale_automate/models/stock_picking.py
+++ sale_automate/models/stock_picking.py
@@ -6,12 +6,12 @@ from odoo.exceptions import UserError
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
-    def button_validate(self):
+    def button_validate_action(self):
         self.ensure_one()
         #-------New instruction to set qty_done------------
         for line in self.move_ids_without_package:
             line.quantity_done = line.product_uom_qty if (line.quantity_done != line.product_uom_qty) else line.quantity_done
 
-        return super().button_validate()
+        return self.button_validate()
     
 
