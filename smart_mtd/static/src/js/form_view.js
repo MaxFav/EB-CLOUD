ca8971c8d78ca5ef5f7c3f710da321b8e0343984
Dan-SmartIT <dan.wareing@smart-ltd.co.uk>
2022-02-18 11:36:45 +0000

    Add MTD module

---
+++ smart_mtd/static/src/js/form_view.js
@@ -0,0 +1,14 @@
+odoo.define('smart_mtd.ActionManager', function (require) {
+    "use strict";
+    
+    //var AbstractAction = require('web.AbstractAction');
+    var core = require('web.core');
+
+    function wizard_keep_open(parent, action){
+        parent.currentDialogController.widget.reload();
+        return false;
+    }
+    core.action_registry.add('wizard_keep_open', wizard_keep_open);
+
+
+});
---
+++ smart_mtd/wizard/__init__.py
@@ -0,0 +1,3 @@
+from . import hmrc_send_wizard
+from . import fuel_scale_wizard
+from . import bad_debt_wizard
