edded377d74eb95a44d813514c8a67c7fefd52e9
AN <99175160+athirapnair1992@users.noreply.github.com>
2022-04-22 14:17:52 +0100

    default warehouse in sale order

--- estella_bartlett/models/sale_order.py
+++ estella_bartlett/models/sale_order.py
@@ -7,6 +7,16 @@ from odoo.exceptions import UserError
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
+    # Remove domain from salesperson field
+    user_id = fields.Many2one('res.users', domain=False)
+
+    @api.onchange('user_id')
+    def onchange_user_id_for_warehouse(self):
+        # Ticket 25712: Ensure correct warehouse is set
+        super(SaleOrder, self).onchange_user_id()
+        if self.partner_id and self.partner_id.x_studio_field_EpOAQ:
+            self.warehouse_id = self.partner_id.x_studio_field_EpOAQ
+
     @api.onchange("partner_id")
     def onchange_partner_id(self):
         """
