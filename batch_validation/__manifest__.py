fde336c1db0890880dd0719e3ff7a657b44a890f
MaxFav <49093951+MaxFav@users.noreply.github.com>
2024-01-15 15:41:21 +0000

    Update __manifest__.py

--- batch_validation/__manifest__.py
+++ batch_validation/__manifest__.py
@@ -3,10 +3,8 @@
     'version': "1.0",
     'license': "LGPL-3",
     'depends': ['base','sale'],
-    'author': "Jamie"
-    'description': """
-    Batch confirmation for quotations
-    """,
+    'author': "Jamie",
+    'description': "Batch confirmation for quotations",
     'data': [
         "actions/sale_order.xml"
     ],
