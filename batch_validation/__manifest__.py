6b2cb263441d1372f2e04a92dcbda1508d73a8f0
MaxFav <49093951+MaxFav@users.noreply.github.com>
2024-01-15 15:32:43 +0000

    Add files via upload

---
+++ batch_validation/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Batch Validation",
+    'version': "1.0",
+    'license': "LGPL-3",
+    'depends': ['base','sale'],
+    'author': "Jamie"
+    'description': """
+    Batch confirmation for quotations
+    """,
+    'data': [
+        "actions/sale_order.xml"
+    ],
+}
