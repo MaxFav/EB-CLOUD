dd20e503b1ea135bdf52e3a2cac609687bef25b4
MaxFav <49093951+MaxFav@users.noreply.github.com>
2024-01-18 16:40:31 +0000

    Delete estella_bartlett directory

--- estella_bartlett/report/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import account_report
-from . import financial_report
-from . import invoice_report
-from . import sale_report
--- estella_bartlett/wizard/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import bulk_return
-from . import stock_picking_return
