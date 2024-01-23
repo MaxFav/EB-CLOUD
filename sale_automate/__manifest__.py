0b28b8d6f1f6cf5b46c126b7f874515a6a688057
MaxFav <49093951+MaxFav@users.noreply.github.com>
2024-01-23 16:04:36 +0000

    add sale_automate

---
+++ sale_automate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': "EB Sale Automate",
+    'version': "1.0",
+    'license': "LGPL-3",
+    'depends': ['base','sale','stock','sale_management'],
+    'author': "Jamie",
+    'description': "Automate Module",
+    'data': [
+        "actions/sale_order.xml"
+    ],
+}
---
+++ sale_automate/models/sale_order.py
@@ -0,0 +1,25 @@
+from odoo import models, fields, api, _
+import pytz
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    def _prepare_invoice(self):
+        invoice_vals = super()._prepare_invoice()
+        tz_name = pytz.timezone(self.env.context.get('tz') or self.env.user.tz or 'UTC')
+        if self.commitment_date and self.env.context.get('set_quantity_done_from_cron'):
+            invoice_vals['invoice_date'] = self.commitment_date.astimezone(tz_name).date()
+        return invoice_vals
+
+    def confirm_and_process_to_draft(self):
+        for rec in self:
+            self.env.cr.execute('SAVEPOINT validate_batch_validation')
+            try:
+                rec.action_confirm()
+                for picking in rec.picking_ids:
+                    picking.action_assign()
+                    picking.with_context(set_quantity_done_from_cron=True).button_validate()
+                rec.with_context(set_quantity_done_from_cron=True)._create_invoices()
+                self.env.cr.commit()
+            except Exception as e:
+                self.env.cr.execute('ROLLBACK TO SAVEPOINT validate_batch_validation')
