56f9dff133277e773729b6a7a91227fcc7466660
MaxFav <49093951+MaxFav@users.noreply.github.com>
2024-01-24 15:25:12 +0000

    Add files via upload

--- estella_bartlett/__manifest__.py
+++ estella_bartlett/__manifest__.py
@@ -2,7 +2,7 @@
     'name': "Estella Bartlett",
     'version': "1.0",
     'license': "LGPL-3",
-    'depends': ['base','stock','sale_management','web_studio','point_of_sale','account_accountant','purchase','hr','boards'],
+    'depends': ['base','stock','sale_management','web_studio','point_of_sale','account_accountant','purchase','hr'],
     'author': "Jamie",
     'description': "Automate Module",
     'data': [       
