6e0eb9dfd8088d052ec1c749658d83aff4bd50cb
MaxFav <49093951+MaxFav@users.noreply.github.com>
2024-01-26 22:09:35 +0000

    Update __manifest__.py

--- estella_bartlett/__manifest__.py
+++ estella_bartlett/__manifest__.py
@@ -1,11 +1,17 @@
 {
-    'name': "Estella Bartlett",
-    'description': "Estella Bartlett Addons Module",
-    'license': "LGPL-3",
-    'version': "1.0",
-    'author': "Jamie",
-    'depends': ['base','web_studio','account_accountant','purchase','sale_quotation_to_invoice','sale_extended_reporting'],
-    'data': [       
+    "name": "Estella Bartlett",
+    "description": "Estella Bartlett Addons Module",
+    "license": "LGPL-3",
+    "version": "1.0",
+    "author": "Jamie",
+    "depends": [
+        "base",
+        "web_studio",
+        "account_accountant",
+        "purchase",
+        "sale_quotation_to_invoice",
+        "sale_extended_reporting",
+        "partner_territory_and_category",
     ],
-    'auto_install': True
+    "data": [],
 }
