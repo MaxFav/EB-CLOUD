5c9f27cc70349385ba0310fdf5e6770068b19c21
MaxFav <49093951+MaxFav@users.noreply.github.com>
2024-01-26 22:08:44 +0000

    Add partner_territory_and_category

---
+++ partner_territory_and_category/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "EB Partner Territory and Category Fields",
+    "description": """
+        Add territory and category from partner_id in invoice reports""",
+    "license": "LGPL-3",
+    "version": "1.0",
+    "author": "Jamie",
+    "depends": ["base", "account", "sale", "sale_module_reporting"],
+    "data": [],
+}
---
+++ partner_territory_and_category/models/__init__.py
@@ -0,0 +1,5 @@
+from . import res_partner
+from . import sale_order
+from . import invoice_report
+from . import sale_report
+from . import account_move
