9906b5e419a2355182cd683949a6d64d9ba2d155
MaxFav <49093951+MaxFav@users.noreply.github.com>
2024-01-27 01:31:24 +0000

    Delete sale_extended_reporting directory

--- sale_extended_reporting/__manifest__.py
+++
@@ -1,9 +0,0 @@
-{
-    "name": "EB Extended Sales Reporting",
-    "description": "Extended Sales Reporting Module",
-    "license": "LGPL-3",
-    "version": "1.0",
-    "author": "Jamie",
-    "depends": ["base","sale", "sale_stock", "account"],
-    "data": [],
-}
--- sale_extended_reporting/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import sale_order_line
-from . import sale_report
-from . import account_move
-from . import invoice_report
-from . import res_partner
-from . import sale_order
