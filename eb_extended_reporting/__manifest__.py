c28fce305ed9966271721f1072b73aa9cbd36b20
MaxFav <49093951+MaxFav@users.noreply.github.com>
2024-01-27 09:46:23 +0000

    Add files via upload

--- eb_extended_reporting/__manifest__.py
+++ eb_extended_reporting/__manifest__.py
@@ -4,6 +4,14 @@
     "license": "LGPL-3",
     "version": "1.0",
     "author": "Jamie",
-    "depends": ["base", "sale", "sale_stock", "account", "contacts", "contacts_enterprise"],
+    "depends": [
+        "base",
+        "sale",
+        "sale_stock",
+        "account",
+        "account_accountant",
+        "contacts",
+        "contacts_enterprise",
+    ],
     "data": [],
 }
--- eb_extended_reporting/models/res_partner.py
+++ eb_extended_reporting/models/res_partner.py
@@ -4,6 +4,8 @@ from odoo import fields, models
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
+    territory_id = fields.Many2one("account.analytic.account", string="Territory")
+
     territory = fields.Selection(
         string="Territory", selection=[("UK", "UK"), ("EU", "EU"), ("USA", "USA"), ("ROW", "ROW")]
     )
