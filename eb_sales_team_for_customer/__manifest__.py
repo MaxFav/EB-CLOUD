509a57ba9d8576ed0f52a9a3c5602613a3377d8e
MaxFav <49093951+MaxFav@users.noreply.github.com>
2024-01-29 21:00:34 +0000

    Update __manifest__.py

--- eb_sales_team_for_customer/__manifest__.py
+++ eb_sales_team_for_customer/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "EB Sales Teams for Customer",
-    "description": "Sales Teams for Customer Module ",
+    "description": "Sales Teams for Customer Module",
     "license": "LGPL-3",
     "version": "1.0",
     "author": "Jamie",
