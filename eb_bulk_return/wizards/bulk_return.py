941ee888ddbb4da4cc7300dfa9dbeff6d2a28291
MaxFav <49093951+MaxFav@users.noreply.github.com>
2024-01-29 12:00:59 +0000

    Add files via upload

--- eb_bulk_return/wizards/bulk_return.py
+++ eb_bulk_return/wizards/bulk_return.py
@@ -4,9 +4,21 @@ from odoo.exceptions import UserError
 
 class BulkReturn(models.TransientModel):
     _name = "bulk.return"
+    _description = "Estella Bartlett Returns"
 
     partner_id = fields.Many2one("res.partner", string="Customer", required=True)
     return_line_ids = fields.One2many(
         "bulk.return.line", "return_id", string="Return Lines", required=True
     )
     scrap_all = fields.Boolean(string="Scrap All")
+
+class BulkReturnLine(models.TransientModel):
+    _name = "bulk.return.line"
+    _description = "Bulk Return Line"
+
+    return_id = fields.Many2one("bulk.return")
+    returned_product_id = fields.Many2one(
+        "product.product", string="Returned Product", required=True
+    )
+    quantity = fields.Float(string="Quantity", required=True)
+    scrap_product = fields.Boolean(string="Scrap")
