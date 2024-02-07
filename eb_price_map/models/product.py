ddd1d3e6a1f39f734779b294efa12b3c0896fd79
Jamie Ellisson <jamie@estellabartlett.com>
2024-02-07 14:31:32 +0000

    Added usd field

--- eb_price_map/models/product.py
+++ eb_price_map/models/product.py
@@ -3,10 +3,16 @@ from odoo import fields, models, api
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    eur_rrp = fields.Monetary(string="Euro RRP",compute="_compute_eur_rrp",store=True)
+    euro_rrp = fields.Monetary(string="Euro RRP",compute="_compute_euro_rrp",store=True)
+    currency_euro = fields.Many2one('res.currency',string="Euro", default=1)
+    usd_rrp = fields.Monetary(string="Euro RRP",compute="_compute_usd_rrp",store=True,currency_field="currency_euro")
 
     @api.depends('list_price')
     def _compute_eur_rrp(self):
         for record in self:
-            record.eur_rrp = record.list_price * 2
+            record.euro_rrp = record.list_price * 2
 
+    @api.depends('list_price')
+    def _compute_usd_rrp(self):
+        for record in self:
+            record.usd_rrp = record.list_price / 2
