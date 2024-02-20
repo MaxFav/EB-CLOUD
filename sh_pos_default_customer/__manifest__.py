794770a48eb80d1cdcc1fb6c68fca861bd3871e5
Jamie Ellisson <jamie@estellabartlett.com>
2024-02-20 10:32:11 +0000

    Updated POS Modules

--- sh_pos_default_customer/__manifest__.py
+++ sh_pos_default_customer/__manifest__.py
@@ -1,12 +1,12 @@
 # Part of Softhealer Technologies.
 # Copyright (C) Softhealer Technologies.
 {
-    "name": "POS Default Customer",
+    "name": "Point Of Sale Default Customer",
     "author": "Softhealer Technologies",
     "website": "https://www.softhealer.com",
     "support": "support@softhealer.com",
     "category": "Point Of Sale",
-    "summary": "Point Of Sale Default Customer",
+    "summary": "Point Of Sale Default Customers, POS Default Customer, Default Customer on POS, Point Of Sale Bydefault Customer, POS Bydefault Customer, POS Customers Odoo",
     "description": """In some business, there are walking customers who buy small things and to manage that kind of customers order difficult and make speed slow for order processing, This module useful to set default customer or walking customer in pos. It will be the default selected customer after pos load, next order button, new order.""",
     "version": "16.0.1",
     "depends": ["point_of_sale"],
--- sh_pos_default_invoice/__manifest__.py
+++ sh_pos_default_invoice/__manifest__.py
@@ -1,12 +1,12 @@
 # Copyright (C) Softhealer Technologies.
 # Part of Softhealer Technologies.
 {
-    "name": "POS Default Invoice",
+    "name": "POS Default Invoice | Point Of Sale Default Invoice",
     "author": "Softhealer Technologies",
     "website": "https://www.softhealer.com",
     "support": "support@softhealer.com",
     "category": "Point Of Sale",
-    "summary": "Point Of Sale Default Invoice",
+    "summary": "Point Of Sale Default Invoices, POS Default Invoice, Default Invoice on POS, Point Of Sale Bydefault Invoice, POS Bydefault Invoice, POS Invoices Odoo",
     "description": """This module activates the invoice button by default in the POS. Here the invoice button is selected so when an order placed invoice generates automatically.""",
     "version": "16.0.1",
     "depends": ['base', 'point_of_sale'],
