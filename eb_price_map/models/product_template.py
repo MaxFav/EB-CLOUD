4cfa175049f9a71b7a3a11b6159e7fcde19cfee0
Jamie Ellisson <jamie@estellabartlett.com>
2024-02-22 15:12:20 +0000

    Revert "Adding GBP field"
    This reverts commit 2954351001de67b5c66164a58ad32ab6522dbde8.
    
    "Revert change"

--- eb_price_map/models/product_template.py
+++ eb_price_map/models/product_template.py
@@ -3,12 +3,10 @@ from odoo import models, fields, api
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    gbp_id = fields.Many2one('res.currency',string="GBP",default=lambda self: self.env.ref('base.GBP'))
     eur_id = fields.Many2one('res.currency',string="EUR",default=lambda self: self.env.ref('base.EUR'))
     usd_id = fields.Many2one('res.currency',string="USD",default=lambda self: self.env.ref('base.USD'))
     aud_id = fields.Many2one('res.currency',string="AUD",default=lambda self: self.env.ref('base.AUD'))
     cad_id = fields.Many2one('res.currency',string="CAD",default=lambda self: self.env.ref('base.CAD'))
-    gbp_rrp = fields.Monetary(string="GBP RRP",currency_field="gbp_id",store=True)
     eur_rrp = fields.Monetary(string="EUR RRP",currency_field="eur_id",store=True,compute="_compute_prices")
     usd_rrp = fields.Monetary(string="USD RRP",currency_field="usd_id",store=True,compute="_compute_prices")
     aud_rrp = fields.Monetary(string="AUD RRP",currency_field="aud_id",store=True,compute="_compute_prices")
