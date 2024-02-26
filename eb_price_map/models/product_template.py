63b20874d5fc5b4a08ef69339adafb35ee031487
JamieEllisson <jamieellisson@gmail.com>
2024-02-26 12:04:14 +0000

    Adding WSP fields to product template

--- eb_price_map/models/product_template.py
+++ eb_price_map/models/product_template.py
@@ -12,7 +12,12 @@ class ProductTemplate(models.Model):
     eur_rrp = fields.Monetary(string="EUR RRP",currency_field="eur_id",store=True,compute="_compute_prices")
     usd_rrp = fields.Monetary(string="USD RRP",currency_field="usd_id",store=True,compute="_compute_prices")
     aud_rrp = fields.Monetary(string="AUD RRP",currency_field="aud_id",store=True,compute="_compute_prices")
-    cad_rrp = fields.Monetary(string="CAD RRP",currency_field="cad_id",store=True,compute="_compute_prices")
+    cad_rrp = fields.Monetary(string="CAD RRP",currency_field="cad_id",store=True,compute="_compute_prices")   
+    gbp_wsp = fields.Monetary(string="GBP WSP",currency_field="gbp_id",compute="_compute_prices")
+    eur_wsp = fields.Monetary(string="EUR WSP",currency_field="eur_id",compute="_compute_prices")
+    usd_wsp = fields.Monetary(string="USD WSP",currency_field="usd_id",compute="_compute_prices")
+    aud_wsp = fields.Monetary(string="AUD WSP",currency_field="aud_id",compute="_compute_prices")
+    cad_wsp = fields.Monetary(string="CAD WSP",currency_field="cad_id",compute="_compute_prices")
     price_map_id = fields.Many2one("price.map",string="Price Map Id",store=True,compute="_compute_prices")
 
     @api.depends("gbp_rrp")
@@ -20,14 +25,16 @@ class ProductTemplate(models.Model):
         try:         
             self.price_map_id = self.env["price.map"].search([("gbp","=",self.gbp_rrp)])              
             self.eur_rrp = self.price_map_id.eur
-            self.usd_rrp = self.price_map_id.usd 
+            self.usd_rrp = self.price_map_id.usd
             self.aud_rrp = self.price_map_id.aud
             self.cad_rrp = self.price_map_id.cad
             self.list_price = self.gbp_rrp / 1.2
+            self.gbp_wsp = self.gbp_rrp / 2.5
+            self.eur_wsp = self.eur_rrp / 2.5
+            self.usd_wsp = self.usd_rrp / 2.5
+            self.aud_wsp = self.aud_rrp / 2.2
+            self.cad_wsp = self.cad_rrp / 2.5  
         except:
-            self.eur_rrp = 0
-            self.usd_rrp = 0  
-            self.aud_rrp = 0
-            self.cad_rrp = 0
+            pass
 
     
