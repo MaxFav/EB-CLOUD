f35d37cb36c0d55341afbfe733e4e0c0f6ff3f31
JamieEllisson <jamieellisson@gmail.com>
2024-03-14 19:29:01 +0000

    test: adding eb_price_map

---
+++ eb_price_map/models/__init__.py
@@ -0,0 +1,4 @@
+from . import price_map
+from . import pricelist_item
+from . import product_template
+from . import sale_order
---
+++ eb_price_map/models/product_template.py
@@ -0,0 +1,30 @@
+from odoo import models, fields, api
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    gbp_id = fields.Many2one('res.currency',string="GBP",default=lambda self: self.env.ref('base.GBP'))
+    eur_id = fields.Many2one('res.currency',string="EUR",default=lambda self: self.env.ref('base.EUR'))
+    usd_id = fields.Many2one('res.currency',string="USD",default=lambda self: self.env.ref('base.USD'))
+    aud_id = fields.Many2one('res.currency',string="AUD",default=lambda self: self.env.ref('base.AUD'))
+    cad_id = fields.Many2one('res.currency',string="CAD",default=lambda self: self.env.ref('base.CAD'))
+    gbp_rrp = fields.Monetary(string="GBP RRP",currency_field="gbp_id",store=True)
+    eur_rrp = fields.Monetary(string="EUR RRP",currency_field="eur_id",store=True,compute="_compute_prices")
+    usd_rrp = fields.Monetary(string="USD RRP",currency_field="usd_id",store=True,compute="_compute_prices")
+    aud_rrp = fields.Monetary(string="AUD RRP",currency_field="aud_id",store=True,compute="_compute_prices")
+    cad_rrp = fields.Monetary(string="CAD RRP",currency_field="cad_id",store=True,compute="_compute_prices")       
+    price_map_id = fields.Many2one("price.map",string="Price Map Id",store=True,compute="_compute_prices")
+
+    @api.depends("gbp_rrp")
+    def _compute_prices(self):
+        for product in self:         
+            product.price_map_id = self.env["price.map"].search([("gbp","=",product.gbp_rrp)])              
+            product.eur_rrp = product.price_map_id.eur
+            product.usd_rrp = product.price_map_id.usd
+            product.aud_rrp = product.price_map_id.aud
+            product.cad_rrp = product.price_map_id.cad
+            product.list_price = product.gbp_rrp / 1.2
+           
+     
+
+    
