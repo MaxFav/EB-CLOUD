87e6f70df5971394ef4601c0f0cacfd6c192b993
Jamie Ellisson <jamie@estellabartlett.com>
2024-03-14 15:57:44 +0000

    test: rename module and add eb_bulk_returns

---
+++ eb_bulk_returns/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': "EB Bulk Returns",
+    'version': '1.0',    
+    'author': "Jamie",
+    'license': "LGPL-3",
+    'category': 'Estella Bartlett',    
+    'depends': ['base','account','sale','stock','sale_management','account_accountant'],
+    'data': [], 
+}
