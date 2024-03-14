f777a70f71d3e4ce7124a8e3bdc576f37ac3cf33
Jamie Ellisson <jamie@estellabartlett.com>
2024-03-14 13:29:20 +0000

    test: getting past dev branch errors

---
+++ Module/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "A Module",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Author Name",
+    'category': 'Category',
+    'description': """
+    Description text
+    """,
+    # data files always loaded at installation
+    'data': [
+        
+    ],
+    # data files containing optionally loaded demonstration data
+   
+}
--- eb_sales_quotation_to_invoice/models/res_partner.py
+++
@@ -1,7 +0,0 @@
-from odoo import api, fields, models, _
-
-
-class ResPartner(models.Model):
-    _inherit = "res.partner"
-
-    analytic_account_id = fields.Many2one('account.analytic.account', string="Analytic Account")
