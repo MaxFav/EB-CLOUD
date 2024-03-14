470aa78aba613ee42afc17c7ce583502d50d9dd7
Jamie Ellisson <jamie@estellabartlett.com>
2024-03-14 15:40:44 +0000

    test: confirm SO and process to invoice

--- eb_module/models/sale_order.py
+++ eb_module/models/sale_order.py
@@ -6,13 +6,13 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     def confirm_and_process_to_draft(self):
-        for sale in self:            
-            try:
+        for sale in self:
+            if sale.state not in ["sale"]:         
                 sale.action_confirm()
-                picking = sale.picking_ids[0]                
-                for move in picking:
-                    move.quantity = move.product_uom_qty
-                picking.button_validate()
+            if sale.picking_ids:
+                for picking in sale.picking_ids:               
+                    for move in picking:
+                        move.quantity = move.product_uom_qty
+                    picking.button_validate()
                 sale._create_invoices()             
-            except Exception as e:
-                raise UserError(e)
+        
