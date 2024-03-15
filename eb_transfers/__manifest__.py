f2c8f02036cfe407334ffde5f71af4796ac775e6
Jamie Ellisson <jamie@estellabartlett.com>
2024-03-15 10:47:34 +0000

    test: eb_transfers

---
+++ eb_transfers/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': "EB Sales Quotation to Invoice",
+    'version': '1.0',    
+    'author': "Jamie",
+    'license': "LGPL-3",
+    'category': 'Estella Bartlett',    
+    'depends': ['base','account','sale','stock','sale_management'],
+    'data': [], 
+}
