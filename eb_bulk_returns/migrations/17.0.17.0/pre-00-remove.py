4b366a2accf0c53e56513f2154e9f06b86398a0b
Jamie Ellisson <jamie@estellabartlett.com>
2024-03-15 13:54:57 +0000

    V17 Upgrade

--- eb_bulk_returns/migrations/17.0.17.0/pre-00-remove.py
+++ eb_bulk_returns/migrations/17.0.17.0/pre-00-remove.py
@@ -12,3 +12,4 @@ def migrate(cr,version):
     util.remove_field(cr,"l10n_uk.vat.obligation","submission_message")
     util.remove_field(cr,"l10n_uk.vat.obligation","report_attachment_name")
     util.remove_field(cr,"l10n_uk.vat.obligation","report_attachment_id")
+    util.remove_record(cr,"studio_customization.default_search_view__0bc837d7-719c-4759-a166-97a915f9e43f")
