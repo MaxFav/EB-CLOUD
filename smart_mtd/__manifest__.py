8070fd866d76e30f5487d845176ec31e055ae450
Jamie Ellisson <jamie@estellabartlett.com>
2024-03-15 12:19:52 +0000

    V17 Upgrade

---
+++ smart_mtd/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Smart MTD',
+    'author': 'Smart IT',
+    'version': '1.0',
+    'summary': 'Changes to the MTD Module',
+    'description': 'Changes to the MTD Module',
+    'category': 'Other',
+    'sequence': 1,
+    'depends': ['l10n_uk', 'l10n_uk_reports', 'account_reports', 'account'],
+    'installable': True,
+    'auto_install': False,
+    'data': [ 
+        'security/ir.model.access.csv',
+    ],
+    "license": "LGPL-3",
+}
