f573669079beee62c14165b4e9696919d93d65fb
Jamie Ellisson <jamie@estellabartlett.com>
2024-03-15 13:42:17 +0000

    V17 Upgrade

--- eb_bulk_returns/migrations/17.0.17.0/pre-00-remove.py
+++ eb_bulk_returns/migrations/17.0.17.0/pre-00-remove.py
@@ -1,7 +1,9 @@
 from odoo.upgrade import util
 
 def migrate(cr,version):
-    util.remove_model(cr,"x_store") 
+    util.remove_model(cr,"x_store")
+    util.remove_model(cr,"x_display") 
+    util.remove_model(cr,"x_eb")
     util.remove_model(cr,"smart_mtd.general_settings")
     util.move_field_to_module(cr,"res.partner","warehouse_id","eb_bulk_returns","eb_transfers")   
     util.remove_field(cr,"account.move","bad_debt_enabled")
