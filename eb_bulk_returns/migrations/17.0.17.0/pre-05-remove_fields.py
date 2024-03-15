71ce0816cfd05f66e82633a39ea307aac4fa31a9
Jamie Ellisson <jamie@estellabartlett.com>
2024-03-15 15:27:42 +0000

    V17 Upgrade

--- eb_bulk_returns/migrations/17.0.17.0/pre-05-remove_fields.py
+++ eb_bulk_returns/migrations/17.0.17.0/pre-05-remove_fields.py
@@ -2,6 +2,10 @@ from odoo.upgrade import util
 
 def migrate(cr,version):
     util.remove_field(cr,"account.move","bad_debt_enabled")
+    util.remove_field(cr,"account.move","territory")
+    util.remove_field(cr,"account.move","category")
+    util.remove_field(cr,"sale.report","territory")
+    util.remove_field(cr,"sale.report","category")
     util.remove_field(cr,"account.move.line","hmrc_submitted")
     util.remove_field(cr,"account.move.line","date_on_vat_return")
     util.remove_field(cr,"l10n_uk.vat.obligation","submission_message")
