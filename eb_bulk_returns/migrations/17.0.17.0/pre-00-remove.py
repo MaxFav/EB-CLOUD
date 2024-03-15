64335bd708fb5cf7010185a01463f17280371847
Jamie Ellisson <jamie@estellabartlett.com>
2024-03-15 11:57:57 +0000

    test: update migration script

--- eb_bulk_returns/migrations/17.0.17.0/pre-00-remove.py
+++ eb_bulk_returns/migrations/17.0.17.0/pre-00-remove.py
@@ -2,5 +2,8 @@ from odoo.upgrade import util
 
 def migrate(cr,version):
     util.remove_model(cr,"x_store")
+    util.remove_model(cr,"smart_mtd.general_settings")
+    util.remove_model(cr,"smart_mtd.fuel.scale.wizard")
+    util.remove_model(cr,"smart_mtd.bad.debt.wizard")
     util.remove_module(cr,"smart_mtd")
     
