9fe457ac37000e8ee053c54c1321dce566dc1585
Jamie Ellisson <jamie@estellabartlett.com>
2024-03-21 11:59:34 +0000

    V17 Upgrade

--- eb_bulk_returns/migrations/17.0.17.0/pre-00-remove_models.py
+++ eb_bulk_returns/migrations/17.0.17.0/pre-00-remove_models.py
@@ -1,7 +1,6 @@
 from odoo.upgrade import util
 
 def migrate(cr,version):
-    util.remove_module(cr,"product_listing_cart")
     util.remove_model(cr,"x_store")
     util.remove_model(cr,"x_display") 
     util.remove_model(cr,"x_eb")
---
+++ product_listing_cart/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "EB Product Listing Cart",
+    "version": "1.0",
+    "summary": "Add quantity and add to cart button on product listing",
+    "license": "LGPL-3",
+    "author": "Jamie",
+    "depends": ["base", "website_sale"],
+    "data": [
+        
+    ],
+    "license": "LGPL-3",
+}
