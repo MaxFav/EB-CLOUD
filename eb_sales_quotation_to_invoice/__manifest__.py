57410bad080e525ac9ec562dd5f37c79b4310325
Jamie Ellisson <jamie@estellabartlett.com>
2024-03-21 15:09:25 +0000

    V17 Upgrade

--- eb_sales_quotation_to_invoice/__manifest__.py
+++ eb_sales_quotation_to_invoice/__manifest__.py
@@ -8,10 +8,7 @@
     "application": True, 
     'depends': ['base','account','sale','stock','sale_management','account_accountant'],
     'data': [
-        "data/sale_order_data.xml",
-        "data/account_move_report_eb_invoice.xml",
-        "data/account_move_report_eb_invoice_barcode.xml",
-        "data/account_move_reports.xml"
+        "data/sale_order_data.xml",        
     ],
     "installable": True
 }
