3ac03f154201bf6bf2e9787658b4bd7841f86a37
Jamie Ellisson <jamie@estellabartlett.com>
2024-03-22 17:26:06 +0000

    feat: adding eb_misc module

---
+++ eb_misc/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "EB Misc",
+    "version": "1.0",
+    "summary": "Estella Bartlett Small Customisations",
+    "author": "Jamie",
+    "depends": ['base','stock','sale','account'],
+    "data": [],
+    "license": "LGPL-3"
+}
