02dc818338074ace02a11c8ac78171e73f9ba030
JamieEllisson <jamieellisson@gmail.com>
2024-03-23 15:00:39 +0000

    V17 Upgrade

---
+++ eb_misc/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "EB Miscellaneous",
+    "version": "17.0",
+    "license": "LGPL-3",
+    "summary": "Estella Bartlett Small Customisations",
+    "author": "Jamie",
+    'category': 'Customizations',
+    "application": True, 
+    "depends": ['base','stock','sale','account'],
+    "data": [],
+    "installable": True
+}
---
+++ eb_misc/models/product_template.py
@@ -0,0 +1,7 @@
+from odoo import models, fields, api
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    packaging_image = fields.Image("Packaging Image", max_width=1920, max_height=1920)
+    packaging_image_128 = fields.Image("Packaging Image 128",related="packaging_image", max_width=128, max_height=128, store=True)
