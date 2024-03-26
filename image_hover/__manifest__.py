1a9174b7f6427074b8ae415eee49a45dcf2cc977
Jamie Ellisson <jamie@estellabartlett.com>
2024-03-26 12:15:22 +0000

    add: image_hover

---
+++ image_hover/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Image Hover',
+    'version': '1.0',
+    'license': 'LGPL-3',
+    'author': 'Jamie',
+    'depends': ['base','website_sale','product','stock'],
+    'data': [],
+    'installable': True
+}
