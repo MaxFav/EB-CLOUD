c48e9e320313634c38a02db36183b54c58455776
Jamie Ellisson <jamie@estellabartlett.com>
2024-03-26 14:03:27 +0000

    feat: add second image to view

--- image_hover/__manifest__.py
+++ image_hover/__manifest__.py
@@ -5,5 +5,8 @@
     'author': 'Jamie',
     'depends': ['base','website_sale','product','stock'],
     'data': ['views/products_item_view.xml'],
+    'assets': {
+        'web.assets_frontend': ['image_hover/static/src/css/image_hover.scss']
+    },
     'installable': True
 }
