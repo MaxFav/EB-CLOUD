3513ea0dbca5141ac1dff0470537c508ca3d721d
Jamie Ellisson <jamie@estellabartlett.com>
2024-03-26 12:35:47 +0000

    feat: add product image hover view

--- image_hover/__manifest__.py
+++ image_hover/__manifest__.py
@@ -4,6 +4,6 @@
     'license': 'LGPL-3',
     'author': 'Jamie',
     'depends': ['base','website_sale','product','stock'],
-    'data': [],
+    'data': ['views/products_item_view.xml'],
     'installable': True
 }
